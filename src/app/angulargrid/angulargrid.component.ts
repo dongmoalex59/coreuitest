febe5365e141217530b539bfe09be8e94b11fef1
dongmoalex59 <dongmoalex59@gmail.com>
2021-08-22 15:10:56 +0100

    modification du projet

--- src/app/angulargrid/angulargrid.component.ts
+++ src/app/angulargrid/angulargrid.component.ts
@@ -15,70 +15,117 @@ export class AngulargridComponent implements OnInit {
 
   @ViewChild('agGrid') agGrid: AgGridAngular;
   
-  public columnDefs;
   public searchValue;
   public gridApi;
   public columnApi;
-  public defaultColDef;
-  public defaultColGroupDef;
-  public columnTypes;
-  public rowData: [];
+
   
-  constructor(private http: HttpClient) {
-    this.columnDefs = [
-      {
-        headerName: 'Athlete',
-        field: 'athlete',
-      },
-      {
-        headerName: 'Sport',
-        field: 'sport',
-      },
-      {
-        headerName: 'Age',
-        field: 'age',
-        type: 'numberColumn',
-      },
-      {
-        headerName: 'Year',
-        field: 'year',
-        type: 'numberColumn',
-      },
-      {
-        headerName: 'Date',
-        field: 'date',
-        type: ['dateColumn', 'nonEditableColumn'],
-        width: 220,
-      },
-      {
-        headerName: 'Medals',
-        groupId: 'medalsGroup',
-        children: [
-          {
-            headerName: 'Gold',
-            field: 'gold',
-            type: 'medalColumn',
-          },
-          {
-            headerName: 'Silver',
-            field: 'silver',
-            type: 'medalColumn',
-          },
-          {
-            headerName: 'Bronze',
-            field: 'bronze',
-            type: 'medalColumn',
-          },
-          {
-            headerName: 'Total',
-            field: 'total',
-            type: 'medalColumn',
-            columnGroupShow: 'closed',
-          },
-        ],
-      },
-    ];
-    this.defaultColDef = {
+  constructor() {
+   }
+	   
+	columnDefs = [
+		  {
+			headerName: 'Athlete',
+			field: 'athlete',
+			filter:true
+		  },
+		  {
+			headerName: 'Age',
+			field: 'age',
+		  },
+		  {
+			headerName: 'Country',
+			field: 'country'
+		  },
+		  {
+			headerName: 'Year',
+			field: 'year'
+		  },
+		  {
+			headerName: 'Date',
+			field: 'date'
+		  },
+		  {
+			headerName: 'Sport',
+			field: 'sport'
+		  },
+		  {
+			headerName: 'Medals',
+			field: 'medals',
+			children : [
+				{
+					headerName: 'Gold',
+					field: 'gold'
+				},
+				{
+					headerName: 'Silver',
+					field: 'silver'
+				},
+				{
+					headerName: 'Bronze',
+					field: 'bronze'
+				},
+				{
+					headerName: 'Total',
+					field: 'total'
+				}
+			]
+		  }
+			
+	];
+	
+	rowData = [	
+		{athlete:"Michael Phelps",age:23,country:"United States",year:2008,date:"24/08/2008",sport:"Swimming",gold:8,silver:0,bronze:0,total:8},
+		{athlete:"Michael Phelps",age:19,country:"United States",year:2004,date:"29/08/2004",sport:"Swimming",gold:6,silver:0,bronze:2,total:8},
+		{athlete:"Michael Phelps",age:27,country:"United States",year:2012,date:"12/08/2012",sport:"Swimming",gold:4,silver:2,bronze:0,total:6},
+		{athlete:"Natalie Coughlin",age:25,country:"United States",year:2008,date:"24/08/2008",sport:"Swimming",gold:1,silver:2,bronze:3,total:6},
+		{athlete:"Aleksey Nemov",age:24,country:"Russia",year:2000,date:"01/10/2000",sport:"Gymnastics",gold:2,silver:1,bronze:3,total:6},
+		{athlete:"Alicia Coutts",age:24,country:"Australia",year:2012,date:"12/08/2012",sport:"Swimming",gold:1,silver:3,bronze:1,total:5},
+		{athlete:"Missy Franklin",age:17,country:"United States",year:2012,date:"12/08/2012",sport:"Swimming",gold:4,silver:0,bronze:1,total:5},
+		{athlete:"Ryan Lochte",age:27,country:"United States",year:2012,date:"12/08/2012",sport:"Swimming",gold:2,silver:2,bronze:1,total:5},
+		{athlete:"Allison Schmitt",age:22,country:"United States",year:2012,date:"12/08/2012",sport:"Swimming",gold:3,silver:1,bronze:1,total:5},
+		{athlete:"Natalie Coughlin",age:21,country:"United States",year:2004,date:"29/08/2004",sport:"Swimming",gold:2,silver:2,bronze:1,total:5},
+		{athlete:"Ian Thorpe",age:17,country:"Australia",year:2000,date:"01/10/2000",sport:"Swimming",gold:3,silver:2,bronze:0,total:5},
+		{athlete:"Dara Torres",age:33,country:"United States",year:2000,date:"01/10/2000",sport:"Swimming",gold:2,silver:0,bronze:3,total:5},
+		{athlete:"Cindy Klassen",age:26,country:"Canada",year:2006,date:"26/02/2006",sport:"Speed Skating",gold:1,silver:2,bronze:2,total:5},
+		{athlete:"Nastia Liukin",age:18,country:"United States",year:2008,date:"24/08/2008",sport:"Gymnastics",gold:1,silver:3,bronze:1,total:5},
+		{athlete:"Marit Bjørgen",age:29,country:"Norway",year:2010,date:"28/02/2010",sport:"Cross Country Skiing",gold:3,silver:1,bronze:1,total:5},
+		{athlete:"Sun Yang",age:20,country:"China",year:2012,date:"12/08/2012",sport:"Swimming",gold:2,silver:1,bronze:1,total:4},
+		{athlete:"Kirsty Coventry",age:24,country:"Zimbabwe",year:2008,date:"24/08/2008",sport:"Swimming",gold:1,silver:3,bronze:0,total:4},
+		{athlete:"Libby Lenton-Trickett",age:23,country:"Australia",year:2008,date:"24/08/2008",sport:"Swimming",gold:2,silver:1,bronze:1,total:4},
+		{athlete:"Ryan Lochte",age:24,country:"United States",year:2008,date:"24/08/2008",sport:"Swimming",gold:2,silver:0,bronze:2,total:4},
+		{athlete:"Inge de Bruijn",age:30,country:"Netherlands",year:2004,date:"29/08/2004",sport:"Swimming",gold:1,silver:1,bronze:2,total:4},
+		{athlete:"Petria Thomas",age:28,country:"Australia",year:2004,date:"29/08/2004",sport:"Swimming",gold:3,silver:1,bronze:0,total:4},
+		{athlete:"Ian Thorpe",age:21,country:"Australia",year:2004,date:"29/08/2004",sport:"Swimming",gold:2,silver:1,bronze:1,total:4},
+		{athlete:"Inge de Bruijn",age:27,country:"Netherlands",year:2000,date:"01/10/2000",sport:"Swimming",gold:3,silver:1,bronze:0,total:4},
+		{athlete:"Gary Hall Jr.",age:25,country:"United States",year:2000,date:"01/10/2000",sport:"Swimming",gold:2,silver:1,bronze:1,total:4},
+		{athlete:"Michael Klim",age:23,country:"Australia",year:2000,date:"01/10/2000",sport:"Swimming",gold:2,silver:2,bronze:0,total:4},
+		{athlete:"Susie O'Neill",age:27,country:"Australia",year:2000,date:"01/10/2000",sport:"Swimming",gold:1,silver:3,bronze:0,total:4},
+		{athlete:"Jenny Thompson",age:27,country:"United States",year:2000,date:"01/10/2000",sport:"Swimming",gold:3,silver:0,bronze:1,total:4},
+		{athlete:"Pieter van den Hoogenband",age:22,country:"Netherlands",year:2000,date:"01/10/2000",sport:"Swimming",gold:2,silver:0,bronze:2,total:4},
+		{athlete:"An Hyeon-Su",age:20,country:"South Korea",year:2006,date:"26/02/2006",sport:"Short-Track Speed Skating",gold:3,silver:0,bronze:1,total:4},
+		{athlete:"Aliya Mustafina",age:17,country:"Russia",year:2012,date:"12/08/2012",sport:"Gymnastics",gold:1,silver:1,bronze:2,total:4},
+		{athlete:"Shawn Johnson",age:16,country:"United States",year:2008,date:"24/08/2008",sport:"Gymnastics",gold:1,silver:3,bronze:0,total:4},
+		{athlete:"Dmitry Sautin",age:26,country:"Russia",year:2000,date:"01/10/2000",sport:"Diving",gold:1,silver:1,bronze:2,total:4},
+		{athlete:"Leontien Zijlaard-van Moorsel",age:30,country:"Netherlands",year:2000,date:"01/10/2000",sport:"Cycling",gold:3,silver:1,bronze:0,total:4},
+		{athlete:"Petter Northug Jr.",age:24,country:"Norway",year:2010,date:"28/02/2010",sport:"Cross Country Skiing",gold:2,silver:1,bronze:1,total:4},
+		{athlete:"Ole Einar Bjørndalen",age:28,country:"Norway",year:2002,date:"24/02/2002",sport:"Biathlon",gold:4,silver:0,bronze:0,total:4},
+		{athlete:"Janica Kostelic",age:20,country:"Croatia",year:2002,date:"24/02/2002",sport:"Alpine Skiing",gold:3,silver:1,bronze:0,total:4},
+		{athlete:"Nathan Adrian",age:23,country:"United States",year:2012,date:"12/08/2012",sport:"Swimming",gold:2,silver:1,bronze:0,total:3},
+		{athlete:"Yannick Agnel",age:20,country:"France",year:2012,date:"12/08/2012",sport:"Swimming",gold:2,silver:1,bronze:0,total:3},
+		{athlete:"Brittany Elmslie",age:18,country:"Australia",year:2012,date:"12/08/2012",sport:"Swimming",gold:1,silver:2,bronze:0,total:3},
+		{athlete:"Matt Grevers",age:27,country:"United States",year:2012,date:"12/08/2012",sport:"Swimming",gold:2,silver:1,bronze:0,total:3},
+		{athlete:"Ryosuke Irie",age:22,country:"Japan",year:2012,date:"12/08/2012",sport:"Swimming",gold:0,silver:2,bronze:1,total:3},
+		{athlete:"Cullen Jones",age:28,country:"United States",year:2012,date:"12/08/2012",sport:"Swimming",gold:1,silver:2,bronze:0,total:3},
+		{athlete:"Ranomi Kromowidjojo",age:21,country:"Netherlands",year:2012,date:"12/08/2012",sport:"Swimming",gold:2,silver:1,bronze:0,total:3},
+		{athlete:"Camille Muffat",age:22,country:"France",year:2012,date:"12/08/2012",sport:"Swimming",gold:1,silver:1,bronze:1,total:3},
+		{athlete:"Mel Schlanger",age:25,country:"Australia",year:2012,date:"12/08/2012",sport:"Swimming",gold:1,silver:2,bronze:0,total:3}
+	]
+  ngOnInit(): void {
+	
+	
+    /*
+	this.defaultColDef = {
       width: 150,
       editable: true,
       filter: 'agTextColumnFilter',
@@ -117,9 +164,7 @@ export class AngulargridComponent implements OnInit {
         },
       },
     };
-   }
-
-  ngOnInit(): void {
+	*/
     
   }
 
@@ -136,11 +181,12 @@ export class AngulargridComponent implements OnInit {
   onGridReady = (params) => {
     this.gridApi = params.api;
     this.columnApi = params.columnApi;
-    this.http
-    .get("https://www.ag-grid.com/example-assets/olympic-winners.json")
+    /*this.http
+    .get("https://github.com/dongmoalex59/coreuitest/blob/main/olympic-winners.json")
     .subscribe(data => {
       params.api.set.setRowData(data)
     })
+	*/
   }
 
   Quicksearch() {
